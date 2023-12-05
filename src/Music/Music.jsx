c9d102ff07a676e1948db1c425b4b52fd4fd9bdf
Yefer98 <yyefer98@gmail.com>
2023-12-04 19:16:17 -0500

    Endpoint dinamico mediante uso id artista

--- src/Music/Music.jsx
+++ src/Music/Music.jsx
@@ -1,54 +1,64 @@
-import './Music.css'
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { pedirCanciones} from '../services/servicioSpotify'
+import "./Music.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { pedirCanciones } from "../services/servicioSpotify";
 export function Music() {
-    //const parametros = useParams()
-    const [canciones, setCanciones]=useState(null)
-    const [carga,setCarga]=useState(true)
+  const { id } = useParams();
+  const [canciones, setCanciones] = useState(null);
+  const [carga, setCarga] = useState(true);
 
-    
-
-    useEffect(function(){
-        pedirCanciones()
-        .then( function(respuesta){
-            setCanciones(respuesta)
-            setCarga(false)
-            console.log('res: '+respuesta)
+  useEffect(
+    function () {
+      pedirCanciones(id)
+        .then(function (respuesta) {
+          setCanciones(respuesta);
+          setCarga(false);
+          console.log("res: " + respuesta);
         })
-        .catch( function(respuestaError){
-            console.log(respuestaError)
-        })
-    },[])
+        .catch(function (respuestaError) {
+          console.log(respuestaError);
+        });
+    },
+    [id]
+  );
 
-    if(carga) {
-        return(
-            <>
-                <h3>Cargando...</h3>
-            </>
-        )
-    } else{
-        return(
-            <>
-                <div className="container" >
-                    <div className="row row-col-1 row-cols-md-3">
-                        {
-                            canciones.tracks.map(function(cancion){
-                                return  (
-                                    <div key={cancion} className="col">
-                                        <div className="card h-100 shadow">
-                                            <h3>{cancion.name}</h3>
-                                            <img src={cancion.album.images[0].url} alt="" className='img-fluid w-100' />
-                                            <audio src={cancion.preview_url} controls className='w-100 px-2'></audio>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-
-                    </div>
+  if (carga) {
+    return (
+      <>
+        <h3>Cargando...</h3>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="container">
+          <div className="row row-col-1 row-cols-md-3">
+            {canciones.tracks.map(function (cancion) {
+              return (
+                <div key={cancion} className="col">
+                  <div className="card h-100 shadow">
+                    <h3>{cancion.name}</h3>
+                    <img
+                      src={cancion.album.images[0].url}
+                      alt=""
+                      className="img-fluid w-100"
+                    />
+                    {cancion.preview_url ? (
+                      <audio
+                        src={cancion.preview_url}
+                        controls
+                        className="w-100 px-2"
+                      ></audio>
+                    ) : (
+                      <h3>Preview no disponible</h3>
+                    )}
+                  </div>
                 </div>
-            </>
-        )
-
-    }
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
